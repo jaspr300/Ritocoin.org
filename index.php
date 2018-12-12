5e1889458b0e97b2373d0ec25ed5e9c2fb0ecee9
Trevali Hudson <rito@ritocoin.org>
2018-12-12 16:42:17 -0600

    New layout

---
+++ index.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>The Ritocoin Project</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/lighter.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand nav-link" href="#top">Ritocoin</a>
+        </div> <!-- /.navbar-header -->
+
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <!-- <li><a href="#docs" class="nav-link">Whitepaper</a></li> -->
+            <li><a href="#exchanges" class="nav-link">Exchanges</a></li>
+            <li><a href="#connect" class="nav-link">Connect</a></li>
+            <li><a href="#downloads" class="nav-link">Downloads</a></li>
+            <!-- <li><a href="#faqs" class="nav-link">FAQs</a></li> -->
+            <li><a href="#pools" class="nav-link">Pools</a></li>
+            <!-- <li><a href="#development">Development</a></li> -->
+          </ul>
+        </div> <!-- /.navbar-collapse -->
+      </div> <!-- /.container -->
+    </nav> <!-- /.navbar -->
+
+    <div id="top" class="jumbotron">
+      <div class="container">
+        <h1>The Ritocoin Project</h1>
+        <h2>Ritocoin is a blockchain software development project designed to experiment with alternative and additional concepts with ideas based on those begun by the Ravencoin project.</h2></p>
+<P>
+<center><img src="img/ritocoin.png" class="img-responsive" style="max-width:300px;"></center>
+      </div> <!-- /.container -->
+    </div> <!-- /.jumbotron -->
+
+<!--
+    <div class="container">
+      <h3 id="docs" class="subhead">Whitepaper</h3>
+      <div class="row cool_circles">
+        <div class="col-md-12 col-sm-12 cool_circle">
+          <div class="cool_circle-ball">
+            <span class="glyphicon glyphicon-road"></span>
+          </div>
+          <h3><a href="">Click here to read the Ritocoin whitepaper</a></h3>
+        </div>
+
+      </div>
+    </div>
+-->
+
+    <div class="container">
+      <h3 id="exchanges" class="subhead">Exchanges</h3>
+      <div class="row cool_circles">
+        <div class="col-md-6 col-sm-6 cool_circle">
+          <div class="cool_circle-ball">
+            <a href="https://www.safe.trade/"><img src="/img/exchange/safetrade.png" style="max-width:100px;"></a>
+          </div>
+          <h3><a href="https://www.safe.trade/">SafeTrade</a></h3>
+        </div>
+
+        <div class="col-md-6 col-sm-6 cool_circle">
+          <div class="cool_circle-ball">
+            <a href="https://trade.mct.plus/"><img src="/img/exchange/mct.png" style="max-width:100px;"></a>
+          </div>
+          <h3><a href="https://trade.mct.plus/">MCT+</a></h3>
+        </div> 
+
+      </div> 
+    </div> 
+
+    <div class="container">
+      <h3 id="connect" class="subhead">Connect</h3>
+      <div class="row cool_circles">
+        <div class="col-md-3 col-sm-3 cool_circle">
+          <div class="cool_circle-ball">
+            <a href="https://github.com/RitoProject/"><img src="/img/connect/github.png" style="max-width:100px;"></a>
+          </div>
+          <h3><a href="https://github.com/RitoProject/">Github</a></h3>
+        </div> 
+
+        <div class="col-md-3 col-sm-3 cool_circle">
+          <div class="cool_circle-ball">
+            <a href="https://discord.gg/Bueav8s"><img src="/img/connect/discord.png" style="max-width:100px;"></a>
+          </div>
+          <h3><a href="https://discord.gg/Bueav8s">Discord</a></h3>
+        </div> 
+
+        <div class="col-md-3 col-sm-3 cool_circle">
+          <div class="cool_circle-ball">
+            <a href="https://explorer.ritocoin.org/"><span class="glyphicon glyphicon-link"></span></a>
+          </div>
+          <h3><a href="https://explorer.ritocoin.org/">Explorer</a></h3>
+        </div> 
+
+        <div class="col-md-3 col-sm-3 cool_circle">
+          <div class="cool_circle-ball">
+            <a href="https://bitcointalk.org/index.php?topic=5076284.0"><span class="glyphicon glyphicon-bullhorn"></span></a>
+          </div>
+          <h3><a href="https://bitcointalk.org/index.php?topic=5076284.0">[ANN]</a></h3>
+        </div> 
+
+      </div> 
+    </div> 
+
+    <div class="container">
+      <h3 id="downloads" class="subhead">Downloads</h3>
+      <div class="row faqs">
+        <p class="col-md-12 col-sm-12">
+          <strong>Desktop Wallet</strong><br>
+            <span class="glyphicon glyphicon-briefcase"></span>
+            <B><a href="https://github.com/RitoProject/Ritocoin/releases">Ritocoin Desktop Wallet for Windows, Mac or Linux</a></B>
+        </p>
+        <p class="col-md-12 col-sm-12">
+          <strong>Paper Wallet</strong><br>
+            <span class="glyphicon glyphicon-file"></span>
+            <B><a href="https://paper.ritocoin.org/">Ritocoin Paper Wallet Generator</a></B>
+        </p>
+        <p class="col-md-12 col-sm-12">
+          <strong>Android Wallet</strong><br>
+            <span class="glyphicon glyphicon-phone"></span>
+            <B>Coming very soon!</B>
+        </p>
+      </div> 
+    </div> 
+
+<!--
+
+    <div class="container">
+      <h3 id="faqs" class="subhead">Frequently Asked Questions</h3>
+      <div class="row faqs">
+        <p class="col-md-4 col-sm-6">
+          <strong>Lorem ipsum dolor sit amet?</strong><br>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+          nonummy nibh euismod tincidunt ut laoreet dolore magna.  Ut wisi enim
+          ad minim veniam, quis nostrud exerci tation ullamcorper suscipit
+          lobortis nisl ut aliquip ex ea commodo consequat.
+        </p>
+        <p class="col-md-4 col-sm-6">
+          <strong>Lorem ipsum dolor sit amet?</strong><br>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+          nonummy nibh euismod tincidunt ut laoreet dolore magna.  Ut wisi enim
+          ad minim veniam, quis nostrud exerci tation ullamcorper suscipit
+          lobortis nisl ut aliquip ex ea commodo consequat.
+        </p>
+        <p class="col-md-4 col-sm-6">
+          <strong>Lorem ipsum dolor sit amet?</strong><br>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+          nonummy nibh euismod tincidunt ut laoreet dolore magna.  Ut wisi enim
+          ad minim veniam, quis nostrud exerci tation ullamcorper suscipit
+          lobortis nisl ut aliquip ex ea commodo consequat.
+        </p>
+        <p class="col-md-4 col-sm-6">
+          <strong>Lorem ipsum dolor sit amet?</strong><br>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+          nonummy nibh euismod tincidunt ut laoreet dolore magna.  Ut wisi enim
+          ad minim veniam, quis nostrud exerci tation ullamcorper suscipit
+          lobortis nisl ut aliquip ex ea commodo consequat.
+        </p>
+        <p class="col-md-4 col-sm-6">
+          <strong>Lorem ipsum dolor sit amet?</strong><br>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+          nonummy nibh euismod tincidunt ut laoreet dolore magna.  Ut wisi enim
+          ad minim veniam, quis nostrud exerci tation ullamcorper suscipit
+          lobortis nisl ut aliquip ex ea commodo consequat.
+        </p>
+        <p class="col-md-4 col-sm-6">
+          <strong>Lorem ipsum dolor sit amet?</strong><br>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+          nonummy nibh euismod tincidunt ut laoreet dolore magna.  Ut wisi enim
+          ad minim veniam, quis nostrud exerci tation ullamcorper suscipit
+          lobortis nisl ut aliquip ex ea commodo consequat.
+        </p>
+      </div> 
+    </div> 
+
+-->
+
+    <div class="container">
+      <h3 id="pools" class="subhead">Mining Pools</h3>
+
+<?php
+$pools['antares']['img'] = 'antares.png';
+$pools['blockify']['img'] = 'blockify.png';
+$pools['cos']['img'] = 'cos.png';
+$pools['dapool']['img'] = 'dapool.png';
+$pools['focus-pool']['img'] = 'focus-pool.png';
+$pools['icemining']['img'] = 'icemining.png';
+$pools['marspool']['img'] = 'marspool.png';
+$pools['minermore']['img'] = 'minermore.png';
+$pools['miningpatriot']['img'] = 'miningpatriot.png';
+$pools['nosuchpool']['img'] = 'nosuchpool.png';
+$pools['phi-phi-pool']['img'] = 'phi-phi-pool.png';
+$pools['pickaxe']['img'] = 'pickaxe.png';
+
+$pools['antares']['link'] = 'https://antares-pool.club';
+$pools['blockify']['link'] = 'https://blockify.xyz';
+$pools['cos']['link'] = 'https://gos.cx';
+$pools['dapool']['link'] ='https://xd.dapool.me';
+$pools['focus-pool']['link'] ='https://focus-pool.com';
+$pools['icemining']['link'] ='https://icemining.ca';
+$pools['marspool']['link'] ='https://marspool.org';
+$pools['minermore']['link'] ='https://minermore.com/';
+$pools['miningpatriot']['link'] ='https://miningpatriot.com';
+$pools['nosuchpool']['link'] ='https://nosuchpool.cloud';
+$pools['phi-phi-pool']['link'] ='https://phi-phi-pool.com';
+$pools['pickaxe']['link'] ='https://pickaxe.pro';
+?>
+
+      <div class="row cool_circles">
+<?php
+$x = 0;
+if(is_array($pools)) {
+  while (list($pool,$v)=each($pools)) {
+    if ($x % 3 == 0) {
+      print '</div><div class="row cool_circles">';
+    }
+    $img = $pools[$pool]['img'];
+    $link = $pools[$pool]['link'];
+    print '
+    <div class="col-md-4 col-sm-4 cool_circle">
+      <div class="cool_circle-ball">
+        <a href="'.$link.'"><img src="/img/pools/'.$img.'" style="max-width:100px;"></a>
+      </div>
+      <h3><a href="'.$link.'">'.$pool.'</a></h3>
+    </div>
+    ';
+    $x++;
+  }
+}
+?>
+
+      </div> <!-- /.row -->
+    </div> <!-- /.container -->
+
+    <footer>
+      <div class="container clearfix">
+        <p class="pull-left">
+          Provided by the Ritocoin Project
+        </p>
+        <p class="pull-right">
+          <a href="https://ritocoin.org/">Ritocoin.org</a>
+        </p>
+      </div> <!-- /.container -->
+    </footer>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script>
+      $(".nav-link").click(function(e) {
+        e.preventDefault();
+        var link = $(this);
+        var href = link.attr("href");
+        $("html,body").animate({scrollTop: $(href).offset().top - 80}, 500);
+        link.closest(".navbar").find(".navbar-toggle:not(.collapsed)").click();
+      });
+    </script>
+  </body>
+</html>
