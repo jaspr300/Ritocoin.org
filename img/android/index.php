6ac25e988ce576db2880a76a2ece20d6e67f0da3
Traysi <link@traysi.org>
2019-03-13 12:52:03 -0500

    index.php for android

---
+++ img/android/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Ritocoin Android Wallet Screenshots</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/lighter.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <div id="top" class="jumbotron">
+      <div class="container">
+    <h1>Ritocoin Android Wallet Screenshots</h1>
+      </div> <!-- /.container -->
+    </div> <!-- /.jumbotron -->
+
+    <div class="container">
+<center>
+<img src="Screenshot_20190313-112422.png" class="img-responsive"><P>
+<img src="Screenshot_20190313-112446.png" class="img-responsive"><P>
+<img src="Screenshot_20190313-112457.png" class="img-responsive"><P>
+<img src="Screenshot_20190313-112658.png" class="img-responsive"><P>
+<img src="Screenshot_20190313-112714.png" class="img-responsive"><P>
+<img src="Screenshot_20190313-112729.png" class="img-responsive"><P>
+<img src="Screenshot_20190313-112737.png" class="img-responsive"><P>
+<img src="Screenshot_20190313-112933.png" class="img-responsive"><P>
+<img src="Screenshot_20190313-113046.png" class="img-responsive"><P>
+<img src="Screenshot_20190313-113141.png" class="img-responsive"><P>
+<img src="Screenshot_20190313-114614.png" class="img-responsive"><P>
+<img src="Screenshot_20190313-114635.png" class="img-responsive"><P>
+<img src="Screenshot_20190313-114715.png" class="img-responsive"><P>
+
+</center>
+    </div> 
+
+
+
+    <footer>
+      <div class="container clearfix">
+        <p class="pull-left">
+          Provided by the Ritocoin Project
+        </p>
+        <p class="pull-right">
+          <a href="https://ritocoin.org/">Ritocoin.org</a>
+        </p>
+      </div> <!-- /.container -->
+    </footer>
+
+    <script src="/js/jquery.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script>
+      $(".nav-link").click(function(e) {
+        e.preventDefault();
+        var link = $(this);
+        var href = link.attr("href");
+        $("html,body").animate({scrollTop: $(href).offset().top - 80}, 500);
+        link.closest(".navbar").find(".navbar-toggle:not(.collapsed)").click();
+      });
+    </script>
+  </body>
+</html>
